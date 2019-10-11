6996004f83c8369609561da6184fe4c3ad065505
Jahanggir Jaman <jahanggir-jaman@users.noreply.github.com>
2019-10-11 23:32:16 +0600

    ux improvement

--- Find Roots of a Quadratic Equation V2.c
+++ Find Roots of a Quadratic Equation V2.c
@@ -3,28 +3,42 @@
 
 void main()
 {
-   int a,b,c,d;
-   float x1,x2;
- 
-   printf("Input the value of a,b & c : ");
-   scanf("%d%d%d",&a,&b,&c);
-   d=b*b-4*a*c;
-   if(d==0)
-   {
-     printf("Both roots are equal.\n");
-     x1=-b/(2.0*a);
-     x2=x1;
-     printf("First  Root Root1= %f\n",x1);
-     printf("Second Root Root2= %f\n",x2);
-   }
-   else if(d>0)
-	{
-	   printf("Both roots are real and diff-2\n");
-	   x1=(-b+sqrt(d))/(2*a);
-	   x2=(-b-sqrt(d))/(2*a);
-	   printf("First  Root Root1= %f\n",x1);
-	   printf("Second Root root2= %f\n",x2);
-	}
-	else
-	    printf("Root are imeainary;\nNo Solution. \n");
+    int a,b,c,d;
+    float x1,x2;
+
+    printf("Enter coefficients a, b and c\n");
+
+    printf("a: ");
+    scanf("%d",&a);
+
+    printf("b: ");
+    scanf("%d",&b);
+
+    printf("c: ");
+    scanf("%d",&c);
+
+    d=b*b-4*a*c;
+    
+    if(d==0)
+    {
+        printf("Both roots are equal.\n");
+        
+        x1=-b/(2.0*a);
+        x2=x1;
+        
+        printf("Root 1= %f\n",x1);
+        printf("Root 2= %f\n",x2);
+    }
+    
+    else if(d>0)
+    {
+        x1=(-b+sqrt(d))/(2*a);
+        x2=(-b-sqrt(d))/(2*a);
+        
+        printf("Root 1= %f\n",x1);
+        printf("Root 2= %f\n",x2);
+    }
+    else
+        printf("Root are imaginary;\nNo Solution. \n");
 }
+
